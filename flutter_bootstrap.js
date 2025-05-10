702b6155cb31ccd9b02f6c1bf4a65817be922cb4
TheLastFlame <TheLastSight@mail.ru>
2025-05-11 05:25:06 +1000

    Built web
    Branch: master
    Commit: dd0e85282ffabbe91ba87805f814bd131b2ac9ba (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"194fb17d2aac834020a9913569f262967c2272
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1671735314"
+    serviceWorkerVersion: "1887468880"
   }
 });
